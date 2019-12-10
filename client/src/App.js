f68674b21b1b5b74ef976cb8c788aaa4e68a3633
Keagan Barnes <barnes.keagan9@gmail.com>
2019-12-10 15:46:11 -0700

    basic functions working with one model

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import NoMatch from './components/NoMatch';
+import Products from './components/Products';
+import ProductForm from './components/ProductForm';
+import { Route, Switch } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+
+const App = () => (
+  <div>
+    <Navbar />
+    <Container>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/products" component={Products} />
+        <Route exact path="/products/new" component={ProductForm} />
+        <Route component={NoMatch} />
+      </Switch>
+    </Container>
+  </div>
+)
+
+export default App;
---
+++ client/src/components/ProductForm.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import axios from 'axios';
+import { Form, Header, } from "semantic-ui-react";
+
+class ProductForm extends React.Component {
+  defaultValues = { name: "", price: "", description: "", department: "", };
+  state = { ...this.defaultValues, };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const product = { ...this.state, };
+    axios.post("/api/products", product)
+      .then( res => {
+        this.props.history.push("/products");
+      })
+      this.setState({ ...this.defaultValues, });
+  }
+
+  handleChange = (e) => {
+    const { target: { name, value, } } = e;
+    this.setState({ [name]: value, });
+  }
+
+  render() {
+    const { name, price, description, department, } = this.state;
+
+    return (
+      <div>
+        <Header as="h1">New Product</Header>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group widths="equal">
+            <Form.Input
+              label="Name"
+              name="name"
+              placeholder="Name"
+              value={name}
+              onChange={this.handleChange}
+              required
+            />
+            <Form.Input
+              label="Description"
+              name="description"
+              placeholder="Description"
+              value={description}
+              onChange={this.handleChange}
+              required
+            />
+          </Form.Group>
+          <Form.Group widths="equal">
+            <Form.Input
+              label="Department"
+              name="department"
+              placeholder="Department"
+              value={department}
+              onChange={this.handleChange}
+              required
+            />
+            <Form.Input
+              label="Price"
+              name="price"
+              placeholder="Price"
+              type="number"
+              value={price}
+              onChange={this.handleChange}
+              required
+            />
+          </Form.Group>
+          <Form.Button color="green">Submit</Form.Button>
+        </Form>
+      </div>
+    )
+  }
+}
+
+export default ProductForm;
